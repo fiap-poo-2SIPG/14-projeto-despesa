97e81f6dc88af92443dde0e73f3da0b2d029b33c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-21 11:51:02 -0300

    Classe DespesaDAO e TesteDespesa

--- src/main/java/br/fiap/modelo/DespesaDAO.java
+++ src/main/java/br/fiap/modelo/DespesaDAO.java
@@ -2,10 +2,9 @@ package br.fiap.modelo;
 
 import br.fiap.conexao.Conexao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DespesaDAO {
     private PreparedStatement ps;
@@ -16,12 +15,38 @@ public class DespesaDAO {
         sql = "insert into java_despesa values (?, ?, ?, ?, ?)";
         try(Connection connection = Conexao.conectar()) {
             ps = connection.prepareStatement(sql);
-
+            ps.setLong(1, despesa.getId());
+            ps.setString(2, despesa.getDescricao());
+            ps.setDouble(3, despesa.getValor());
+            ps.setDate(4, Date.valueOf(despesa.getData()));
+            ps.setLong(5, despesa.getCategoria().getId());
             ps.execute();
         }
         catch(SQLException e) {
-            System.out.println("erro ao inserir no banco de dados\n" + e);
+            System.out.println("erro ao inserir despesa\n" + e);
         }
+    }
+
+    public List<Despesa> listar() {
+        List<Despesa> lista = new ArrayList<>();
+        sql = "select * from java_despesa";
+        try(Connection connection = Conexao.conectar()) {
+           ps = connection.prepareStatement(sql);
+           rs = ps.executeQuery();
+           while(rs.next()) {
+               Despesa despesa = new Despesa();
+               despesa.setId(rs.getLong("id_despesa"));
+               despesa.setDescricao(rs.getString("descricao"));
+               despesa.setValor(rs.getDouble("valor"));
+               despesa.setCategoria(new Categoria(rs.getLong("id_categoria"), ""));
+               despesa.setData(rs.getDate("data").toLocalDate());
+               lista.add(despesa);
+           }
+        }
+        catch(SQLException e) {
+            System.out.println("erro ao listar despesas\n" + e);
+        }
+        return lista;
     }
 
 }
